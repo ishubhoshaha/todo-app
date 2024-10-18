2d20d6da657cfcaf80c182188261ce34d34fafeb
Shubho Shaha <shubho.shaha@nexgencloud.com>
2024-10-18 17:19:01 +0600

    Complete login and register

--- todo/app/urls.py
+++ todo/app/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', views.login, name='login'),
-    # path('logout/', views.logout, name='logout'),
-    path('register/', views.register, name='register'),
+    path('update/<int:id>', views.update, name='update'),
     path('delete/<int:id>', views.delete, name='delete'),
-    path('update/<int:id>', views.update, name='update')
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register')
 ]
--- todo/app/views.py
+++ todo/app/views.py
@@ -2,47 +2,73 @@ from django.shortcuts import render, redirect
 from .models import todo
 from django.contrib import messages
 
+
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+
+@login_required
 def home(request):
     if request.method == 'POST':
-        try:
-            task = request.POST['task']
-            new_task = todo(name=task)
-            new_task.save()
-            messages.success(request, "Your task has been created successfully!")
-        except Exception as E:
-            messages.error(request, "Error Occured!")
+        new_task = todo(name=request.POST['task'])
+        new_task.save()
+        messages.success(request, "Your task has been saved!")
     all_task = todo.objects.all()
-    context = {'all_task': all_task}
-
+    context = {
+        'all_task': all_task
+    }
     return render(request, 'todo/home.html', context)
 
-def register(request):
-    return render(request, 'todo/register.html', {})
+def update(request, id):
+    task = todo.objects.filter(id=id)
+    if task.first().status == True:
+        task.update(status=False)
+    else:
+        task.update(status=True)
+    return redirect(home)
 
-def login(request):
-    return render(request, 'todo/login.html', {})
 
 def delete(request, id):
-    try:
-        todo.objects.filter(id=id).delete()
-        messages.error(request, "Deleted Successfully!")
-    except Exception as E:
-        messages.error(request, "Error Occured!! Can't delete your task.")
+    todo.objects.filter(id=id).delete()
     return redirect(home)
 
-def update(request, id):
-    try:
-        task = todo.objects.filter(id=id)
-        if task.first().status == True:
-            task.update(status=False)
-            messages.success(request, "Task back to your bucket!")
+def login_view(request, *args, **kwargs):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        asdf = authenticate(request, username=username, password=password)
+        if asdf is not None:
+            login(request, asdf)
+            messages.success(request,"Successfully Login")
+            return redirect(home)
         else:
-            task.update(status=True)
-            messages.success(request, "Bingo! Task Completed.")
+            messages.error(request, "Invalid username or password")
 
-    except Exception as E:
-        messages.error(request, "Error! Can't update your task.")
+    return render(request, 'todo/login.html', {})
 
-    return redirect(home)
+
+def logout_view(request, *args, **kwargs):
+    logout(request)
+    return redirect('login')
+
+
+def register_view(request, *args, **kwargs):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        email = request.POST['email']
+        confirm_pass = request.POST['confirm-password']
+        user_exists = User.objects.filter(username=username).exists()
+        if user_exists:
+            messages.error(request, "username already taken")
+        elif password != confirm_pass:
+            messages.error(request, "Password does not match")
+        else:
+            new_user = User.objects.create_user(username=username, email=email,password=password)
+            new_user.save()
+            return redirect('login')
+
+        print(request.POST)
+    return render(request, 'todo/register.html', {})
